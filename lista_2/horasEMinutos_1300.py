b06c2a7ec442c03a69bbd8a2a9d9efdcb0ebcf2a
Flavio Farias <flavio.farias@ccc.ufcg.edu.br>
2018-04-23 20:43:52 -0300

    horas e minutos e queries

--- lista_2/horasEMinutos_1300.py
+++ lista_2/horasEMinutos_1300.py
@@ -1,3 +1,6 @@
-a = int(input())
-
-print("Y") if a%6==0 else print("N")
+while True:
+    try:
+        a = int(input())
+        print("Y") if a%6==0 else print("N")
+    except EOFError:
+        break
