753d7637f2154c592d7794ce9674762bf77c526a
Flavio <flavio.farias@ccc.ufcg.edu.br>
2019-04-24 08:29:50 -0300

    atualizando

--- lista3/inversionCount.cpp
+++ lista3/inversionCount.cpp
@@ -3,52 +3,74 @@
 using namespace std;
 int INF = 100000001;
 typedef pair<int, int> par;
-par segTree [40000001];
-int arr [10000001];
+int segTree [8000001];
+par arr [2000001];
 int ZERO = 0;
 
-int BiT [10000001];
+par BiT [10000001];
 
-void update(int id, int v){
-    for(int i = id; i < 100000001; (i & -1)){
-        BiT[i] += v;
+bool compare(par p1, par p2){
+    return p1.first > p2.first;
+}
+
+
+void insert(int id, int l, int r, int index){
+    if(index < l || index > r) return;
+    if (l == r && r == index){ 
+        segTree[id] += 1;
+        return;
+    }
+    
+    int m = (l + r) / 2;
+    insert(id*2, l, m, index);
+    insert(id*2+1, m+1, r, index);
+    segTree[id] = segTree[id*2] + segTree[id*2+1];
+}
+
+void update(int id, int l, int r, int v, int pos){
+    if (l == r) segTree[id] += v;
+    else{
+        int m = (l + r) / 2;
+        if (pos <= m) update(id*2, l, m, v, pos);
+        else update(id*2+1, m+1, r, v, pos);
+        segTree[id] = segTree[id*2] + segTree[id*2+1];
     }
 }
 
-int query(int id){
-    int ret = 0;
-    for(int i = id; i < 0; -(i & -1)){
-        ret += BiT[i];
+int query(int id, int l, int r, int x, int y){
+    if (x <= l && r <= y) return segTree[id];
+    else if (y < l || r < x) {
+        return 0;
     }
-    
-    return ret;
+    int m = (l + r) / 2;
+    return query(id*2, l, m, x, y) + query(id*2+1, m+1, r, x, y);
 }
 
 
 int main(){
-    int t, n, num;
-    memset(arr, 0, 10000001);
-
+    int t, n, num, soma;
+    
     cin >> t;
     while(t > 0){
-
+        memset(segTree, 0, 8000001);
+        soma = 0;
         cin >> n;
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i <= n; i++){
             cin >> num;
-
-            cout << query(num) << endl;
-            
-            update(num, 1);
+            arr[i] = par(num, i);
         }
+        sort(begin(arr), end(arr), compare);
 
-        sort(begin(arr), end(arr));
+        for (int i = 0; i < n; i++){
+            soma += query(1, 1, n, 1, arr[i].second);
+            update(1, 1, n, 1, arr[i].first);
+        }
+        cout << soma << endl;
 
 
-
-
-
+        t--;
+     
     }
-
 
 
 }
