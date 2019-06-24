2b3b335945dbd2761d88bd070bb1ce925844056a
Flavio <flavio.farias@ccc.ufcg.edu.br>
2019-06-24 10:18:37 -0300

    beautifulNumbers incompleta

--- lista6/beautifulNumbers.py
+++ lista6/beautifulNumbers.py
@@ -1,7 +1,6 @@
-from math import factorial as fa
 MOD = 1000000007
 D6 = 1000000
-a, b, n = map(int, raw_input().split())
+
 fat = [0] * (D6+2)
 fat[0] = 1
 fat[1] = 1
@@ -10,7 +9,11 @@ def preFat():
     for i in xrange(2,D6+2):
         fat[i] = (fat[i-1] * i) % MOD
 
+
 preFat()
+
+a, b, n = map(int, raw_input().split())
+
 
 def calc(a, b, n):
     i = n
@@ -18,12 +21,14 @@ def calc(a, b, n):
     soma = 0
     while i >= 0 and f <= n:
         numero = a*i + b*f
-        #print a, i, b, f, a*i + b*f
+        print a, i, b, f, numero
         if isB(str(a), str(b), str(numero)):
             #print "haha"
             soma = (soma + qntd(i, f)) % MOD
+            print soma, i, f, qntd(i, f)
         i -= 1
         f += 1
+    if i == f: soma = soma / 2.0
     return soma
 
 
@@ -34,7 +39,7 @@ def qntd(i, f):
 def isB(a, b, n):
     
     setn = set(n)
-    #print n, setn, a, b
+    print n, setn, a, b
     return (len(setn) == 2 and (a in setn and b in setn)) or (len(setn) == 1 and (a in setn or b in setn ))
     
 print int(calc(a, b, n))
