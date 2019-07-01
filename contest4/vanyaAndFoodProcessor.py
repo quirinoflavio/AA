3ab7cf5addb111e1ba1fad560c35e5df255f47e6
Flavio <flavio.farias@ccc.ufcg.edu.br>
2019-07-01 08:22:15 -0300

    educational67

--- contest4/vanyaAndFoodProcessor.py
+++ contest4/vanyaAndFoodProcessor.py
@@ -2,55 +2,16 @@ n, h, k = map(int, raw_input().split())
 
 potatos = map(int, raw_input().split())
 
-usado = 0
-counter = 0
+soma = 0
+res = 0
 
-p = 0
-while p < len(potatos)-1:
-    print p, counter , usado
-    if potatos[p] + usado <= h:
-        usado += potatos[p]
-        if potatos[p+1] + usado > h:
-            if k > usado: 
-                counter += 1
-                usado = 0
-            else:
-                counter += usado / k
-                usado = usado % k
-        p += 1
-        
-    else:
-        if k > usado: 
-            counter += 1
-            usado = 0
-        else:
-            counter += usado / k
-            usado = usado % k
-
-print counter
-if potatos[-1] + usado <= h: 
-    usado += potatos[-1]
-    if k > usado: 
-        counter += 1
-        usado = 0
-    else:
-        counter += usado / k
-        usado = usado % k
-else:
-    if k > usado: 
-        counter += 1
-        usado = 0
-    else:
-        counter += usado / k
-        usado = usado % k
-    usado += potatos[-1]
-    if k > usado: 
-        counter += 1
-        usado = 0
-    else:
-        counter += usado / k
-        usado = usado % k
-        
-
-
-print counter
+for i in xrange(n):
+    soma += potatos[i]
+    if soma > h:
+        soma = potatos[i]
+        res += 1
+    res += soma/k
+    soma = soma%k
+    
+if soma > 0: res += 1
+print res
