0abac85897a5ac2fbf1f6d5935eb3177f6a719d3
Flavio <flavio.farias@ccc.ufcg.edu.br>
2019-09-27 08:20:31 -0300

    Klingon Levels

--- intermediario2/lista4/klingonLevels.cpp
+++ intermediario2/lista4/klingonLevels.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
 
 #define MAXN (int) 1e4 + 1
-
+#define INF (int) 1e7
 using namespace std;
 
 vector<int> notas[MAXN];
 int tam[MAXN];
-
+int queries, divs, qntd, nota, pos, acum, mindif;
 int main(){
-    int queries, divs, qntd, nota, t=0, pos, acum;
+    
     scanf("%d", &divs);
     while(divs != 0){
+        memset(tam, 0, MAXN);
+        
         
         for (int i = 0; i < divs; i++){
             scanf("%d", &qntd);
             tam[i] = qntd;
+            notas[i].clear();
             for(int j = 0; j < qntd; j++){
                 scanf("%d", &nota);
                 notas[i].push_back(nota);
@@ -23,23 +26,21 @@ int main(){
             sort(notas[i].begin(), notas[i].end());
         }
 
-        t = 0;
         acum = 0;
-        for(int tz = )
-        for (int i = 0; i < divs; i++){        
-            for(int j = 0; j < qntd; j++){
-                pos = lower_bound(notas[i].begin(), notas[i].end(), t) - notas[i].begin();
-                if(notas[i][pos] == t){
-                    pos--;
-                }
-                acum += abs( (pos+1) - (pos+1 - tam[i]) );
-                
-                //1 2 2 3 3 3 4 5
-                //0 1 2 3 4 5 6 7
+        mindif = INF;
+        for(int tz = 0; tz <= 1000; tz++){
+            for (int i = 0; i < divs; i++){        
+                    pos = lower_bound(notas[i].begin(), notas[i].end(), tz) - notas[i].begin();
+                    if(pos == 0 || pos == tam[i]) acum += tam[i];    
+                    else acum += abs( (pos) - (tam[i] - pos) );
+                   
             }
+            if (acum < mindif) mindif = acum;
+            acum = 0;
+
         }
 
-        printf("%d\n", acum);
+        printf("%d\n", mindif);
         scanf("%d", &divs);
 
     }
