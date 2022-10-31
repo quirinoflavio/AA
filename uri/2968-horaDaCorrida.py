470ac598ca934c56e4032b402b0c32bba054e980
SasaSales <87549565+SasaSales@users.noreply.github.com>
2022-10-31 18:53:22 -0300

    Adding print

--- uri/2968-horaDaCorrida.py
+++ uri/2968-horaDaCorrida.py
@@ -3,3 +3,4 @@ from math import ceil
 v, n = map(int, raw_input().split())
 
 tmp = v*n
+print("%d %d %d %d %d %d %d %d %d" % (ceil(tmp*0.1), ceil(tmp*0.2), ceil(tmp*0.3), ceil(tmp*0.4), ceil(tmp*0.5), ceil(tmp*0.6), ceil(tmp*0.7), ceil(tmp*0.8), ceil(tmp*0.9)))
